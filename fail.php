8fde9a7ccf86f4d01384fe2b3f906c02e1383510
Sergio <Sergioschimpf@gmail.com>
2025-10-24 08:19:11 -0300

    Add fail.php with error message HTML

---
+++ fail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>fail.php - Sergio Schimpf</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <h1>Fail</h1>
+  <p>Lo siento, algo salió mal.</p>
+</body>
+</html>
