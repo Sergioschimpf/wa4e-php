592f30dde0354695e0787c2cc5dc19c9ea05012d
Sergio <Sergioschimpf@gmail.com>
2025-10-24 08:19:49 -0300

    Add initial HTML structure in check.php

---
+++ check.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>check.php - Sergio Schimpf</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <h1>Check</h1>
+  <p>Todo funciona correctamente.</p>
+</body>
+</html>
